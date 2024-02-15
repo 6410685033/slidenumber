3c4c3f111915f2d7e2dfe31c0bf1ed7bd4ba6f67
Pond <6410685033@student.tu.ac.th>
2024-02-15 09:56:15 +0700

    Initial Commit

---
+++ SlideNumberPuzzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlideNumberPuzzle
+//
+//  Created by Thammasat Thonggamgaew on 15/2/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlideNumberPuzzle/SlideNumberPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideNumberPuzzleApp.swift
+//  SlideNumberPuzzle
+//
+//  Created by Thammasat Thonggamgaew on 15/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SlideNumberPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
